4a774c4dc2c5a0b1c7926bdcda723330d9696d54
AymenBida <aymen.bida.00@gmail.com>
2021-02-18 16:24:19 +0100

    Add delete events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -33,6 +33,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.delete
+    redirect_to @event.user
+  end
+
   private
 
   def event_params
