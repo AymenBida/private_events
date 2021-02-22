f12409a4e06a971c89686e1a44fdd69338af3985
AymenBida <aymen.bida.00@gmail.com>
2021-02-22 17:02:42 +0100

    Add success message when inviting a user

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,14 +3,16 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.new
     @event = Event.find(params[:event_id])
     @attendees = @event.invitations.joins(:attendee).pluck(:username)
-    @invited_users = User.where("users.username != '#{current_user.username}'")
+    @invited_users = User.where.not(username: current_user.username)
       .where.not(username: @attendees).pluck(:username, :id)
   end
 
   def create
     @event = Event.find(params[:event_id])
     @invitation = @event.invitations.build(user_id: params[:invitation][:attendee_id])
+    @invited_user = User.find(params[:invitation][:attendee_id])
     if @invitation.save
+      flash[:success] = "You successfuly invited #{@invited_user.username} !"
       redirect_to @event
     else
       render :new
